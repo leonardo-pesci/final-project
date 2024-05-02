909cae42fbf976d8d4a6e284d215f5deba7e4598
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-05-02 12:10:49 +0200

    nav

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ document.querySelector('#toolsBtn');
 document.querySelector('#aboutBtn');
 document.querySelector('#contactsBtn');
 document.querySelector('#Btn');
+const navBtn = document.querySelectorAll('.nav-item');
+const mainSections = document.querySelectorAll('.mainSection')
 
 
 
@@ -19,7 +21,20 @@ document.querySelector('#Btn');
 
 
 //^ EVENTS
+navBtn.forEach( (btn) => {
+    btn.addEventListener('click', () => {
+        let item = btn.innerText
+        let lowerItem = item.toLowerCase()
+        let list = ['home', 'games', 'tools', 'about', 'contacts']
 
+        mainSections.forEach( (section) => {
+            section.classList.add('hidden')
+        })
+
+        const section = document.querySelector(`#${lowerItem}`)
+        section.classList.remove('hidden')
+    })
+})
 
 
 
