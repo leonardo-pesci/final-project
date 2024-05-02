e5fe9be616381d2e9be0403e1f5b1d21706cbaf5
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-05-02 17:00:49 +0200

    cards

--- src/main.js
+++ src/main.js
@@ -6,17 +6,34 @@ document.querySelector('#aboutBtn');
 document.querySelector('#contactsBtn');
 document.querySelector('#Btn');
 const navBtn = document.querySelectorAll('.nav-item');
-const mainSections = document.querySelectorAll('.mainSection')
+const mainSections = document.querySelectorAll('.mainSection');
+const stars = document.querySelectorAll('.star');
 
 
 
 //^ VARIABLES
-
+let starred = [];
+let todoList = []
+const starredStorage = localStorage.getItem('starred')
+if (starredStorage) starred = JSON.parse(starredStorage)
 
 
 
 //^ FUNCTIONS
+let renderStars = () => {
 
+
+
+    localStorage.setItem('starred', JSON.stringify(starred))
+    console.log(starred)
+
+    stars.forEach( (star, index) => {
+        if (index in starred) star.classList.add('starFull')
+        else star.classList.remove('starFull')
+    })
+}
+
+renderStars()
 
 
 
@@ -33,6 +50,20 @@ navBtn.forEach( (btn) => {
 
         const section = document.querySelector(`#${lowerItem}`)
         section.classList.remove('hidden')
+    })
+})
+
+stars.forEach( (star, index) => {
+    star.addEventListener('click', () => {
+        // star.classList.toggle('starFull')
+        if (index in starred) {
+            const j = starred.indexOf(index)
+            starred.splice(j, 1)
+        } else {
+            starred.push(index)
+        }
+        
+        renderStars()
     })
 })
 
