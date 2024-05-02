7671e4c3e0bd505ca7148c7338290ae3115437b1
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-05-02 11:58:28 +0200

    css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 //^ ELEMENTS
-
+document.querySelector('#homeBtn');
+document.querySelector('#gamesBtn');
+document.querySelector('#toolsBtn');
+document.querySelector('#aboutBtn');
+document.querySelector('#contactsBtn');
+document.querySelector('#Btn');
 
 
 
