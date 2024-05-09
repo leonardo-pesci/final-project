d5aa14024090da60fff7559013b0598888d299da
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-05-09 11:20:57 +0200

    memory

--- src/main.js
+++ src/main.js
@@ -5,25 +5,26 @@ document.querySelector('#toolsBtn');
 document.querySelector('#aboutBtn');
 document.querySelector('#contactsBtn');
 document.querySelector('#Btn');
-const navBtn = document.querySelectorAll('.nav-item');
+const navBtn = document.querySelectorAll('.mainNavItem');
 const mainSections = document.querySelectorAll('.mainSection');
 const stars = document.querySelectorAll('.star');
 
 
 
 //^ VARIABLES
+let todoList = [];
 let starred = [];
-let todoList = []
+let lastSection = 'Home'
 const starredStorage = localStorage.getItem('starred')
 if (starredStorage) starred = JSON.parse(starredStorage)
-
+const lastSectionStorage = localStorage.getItem('lastSection')
+if (lastSectionStorage) lastSection = JSON.parse(lastSectionStorage)
 
 
 //^ FUNCTIONS
 let renderStars = () => {
 
     localStorage.setItem('starred', JSON.stringify(starred))
-    console.log(starred)
 
     stars.forEach( (star, index) => {
         if (starred.includes(index)) stars[index].classList.add('starFull')
@@ -33,22 +34,35 @@ let renderStars = () => {
 
 renderStars()
 
+let setLastSection = (lastSection) => {
+    localStorage.setItem('lastSection', JSON.stringify(lastSection))
+}
 
-
-//^ EVENTS
-navBtn.forEach( (btn) => {
-    btn.addEventListener('click', () => {
-        let item = btn.innerText
+let showSection = (item) => {
+    console.log(item)
         let lowerItem = item.toLowerCase()
         
         mainSections.forEach( (section) => {
             section.classList.add('hidden')
         })
 
+        setLastSection(item)
+
         const section = document.querySelector(`#${lowerItem}`)
         section.classList.remove('hidden')
+}
+
+showSection(lastSection)
+
+
+//^ EVENTS
+navBtn.forEach( (btn) => {
+    btn.addEventListener('click', () => {
+    let item = btn.innerText
+    showSection(item)
     })
 })
+    
 
 stars.forEach( (star, index) => {
     
